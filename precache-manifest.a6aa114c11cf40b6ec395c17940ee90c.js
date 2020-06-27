bb85469d64117d99f65a07f98b6903e2b852e294
Khushi Premal Desai <khushipdesai@berkeley.edu>
2020-06-26 17:13:33 -0700

    Updates

---
+++ precache-manifest.a6aa114c11cf40b6ec395c17940ee90c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ac8526ae77a390498741400d40c75b",
+    "url": "/icecap/index.html"
+  },
+  {
+    "revision": "1ae875f8bc4de8985f02",
+    "url": "/icecap/static/css/main.9e90bdab.chunk.css"
+  },
+  {
+    "revision": "89becbc3f0c24c73f8f6",
+    "url": "/icecap/static/js/2.137c34cd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/icecap/static/js/2.137c34cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae875f8bc4de8985f02",
+    "url": "/icecap/static/js/main.afd3308f.chunk.js"
+  },
+  {
+    "revision": "92e2aee28427d8a5a8c6",
+    "url": "/icecap/static/js/runtime-main.0fe271a5.js"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/icecap/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/icecap/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "792b14289f2337007410282e4b2459d9",
+    "url": "/icecap/static/media/mountain_image.792b1428.jpeg"
+  }
+]);
