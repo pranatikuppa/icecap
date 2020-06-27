e1c2a1dc67ce7e5f250948dafe125b970836fcfb
Khushi Premal Desai <khushipdesai@berkeley.edu>
2020-06-27 13:24:34 -0700

    Added more documentation to some of the UI code

--- src/components/ContactButton.js
+++ src/components/ContactButton.js
@@ -7,6 +7,9 @@ import Button from '@material-ui/core/Button';
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
+/**
+ * Styles to apply to the app bar elements of the page.
+ */
 const appBarStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
@@ -19,21 +22,39 @@ const appBarStyles = makeStyles((theme) => ({
       color: '#6493a1',
       borderColor: '#6493a1',
     }
-  }));
+}));
 
+/**
+ * The ContactButton component created to open a dialog 
+ * within the application for the user to interact with.
+ */
 export default function ContactButton() {
+    /**
+     * Styles and necessary states of the button
+     * to keep track of.
+     */
     const classes = appBarStyles();
-  
     const [open, setOpen] = React.useState(false);
   
+    /**
+     * The handler method that handles when the button
+     * is clicked to open.
+     */
     const handleClickOpen = () => {
       setOpen(true);
     };
   
+    /**
+     * The handler method that handles when the button 
+     * is clicked to close.
+     */
     const handleClose = () => {
       setOpen(false);
     };
   
+    /**
+     * The components that make up the ContactButton.
+     */
     return (
       <div>
         <Button variant="outlined" className={classes.button} onClick={handleClickOpen}>
--- src/components/FileDownloadComponent.js
+++ src/components/FileDownloadComponent.js
@@ -29,9 +29,19 @@ const useStyles = makeStyles((theme) => ({
 
 export default function(props) {
 
+    /**
+     * The styles and states of the FileDownloadComponent that
+     * needs to be kept track of.
+     */
     const classes = useStyles();
     const [currentFileName, setCurrentFileName] = React.useState(props.defaultFileName);
 
+    /**
+     * Helper function that takes in a filename
+     * and validates and cleans it, returning a valid
+     * filename based on the input.
+     * @param {String} filename the inputted filename.
+     */
     function validateFilename(filename) {
         var otherPattern = new RegExp("\\W");
         if (filename.trim() === "" || otherPattern.exec(filename)) {
@@ -49,6 +59,11 @@ export default function(props) {
         }
     }
 
+    /**
+     * The handler method that handles a change to
+     * the text field.
+     * @param {Object} e the event object that the text field returns. 
+     */
     function handleChange(e) {
         var newUnfilteredName = e.target.value;
         var newFilteredName = validateFilename(newUnfilteredName);
@@ -56,6 +71,12 @@ export default function(props) {
         setCurrentFileName(newFilteredName);
     }
 
+    /**
+     * Helper method that performs a download when called
+     * using the filename and the content passed in.
+     * @param {String} filename the name of the file.
+     * @param {String} text the content that the file will contain once downloaded.
+     */
     function download(filename, text) {
         var element = document.createElement('a');
         element.setAttribute('href', 'data:.java;charset=utf-8,' + encodeURIComponent(text));
@@ -69,10 +90,17 @@ export default function(props) {
         document.body.removeChild(element);
     }
 
+    /**
+     * The handler method that performs the download using the 
+     * appropriate variables.
+     */
     function doDownload() {
         download(currentFileName, props.contentList[props.fileIndex]);    
     }
 
+    /**
+     * The components that make up the FileDownloadComponent.
+     */
     return (
         <div className='DownloadTextField' style={{ whiteSpace: 'break-spaces', lineHeight: 4.5 }}>
             <CssTextField 
